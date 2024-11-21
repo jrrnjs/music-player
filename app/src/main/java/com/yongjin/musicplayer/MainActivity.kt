bac1ddff63ea6506ae2aa8a9327e8c8b7014dbde
yongjin.kwon <jrrnjs@gmail.com>
2024-11-21 12:35:27 +0900

    App, Activity Hilt 설정 추가

--- app/src/main/java/com/yongjin/musicplayer/MainActivity.kt
+++ app/src/main/java/com/yongjin/musicplayer/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.yongjin.musicplayer.designsystem.theme.MusicPlayerTheme
 import com.yongjin.musicplayer.feature.library.LibraryScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
