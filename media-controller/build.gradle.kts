cbafc9f7e994b234f7689edbb36d0654c3914e19
yongjin.kwon <jrrnjs@gmail.com>
2024-11-24 22:59:02 +0900

    media-controller 모듈 생성

---
+++ media-controller/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("musicplayer.android.library")
+    id("musicplayer.android.hilt")
+}
+
+android {
+    namespace = "com.yongjin.musicplayer.media"
+}
+
+dependencies {
+    implementation(project(":model"))
+    implementation(libs.androidx.media3.session)
+}
